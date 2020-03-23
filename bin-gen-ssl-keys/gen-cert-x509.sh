99d84c34ecc72c584b2c60f7c2a38dc928bee0fc
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-03-23 11:53:09 +0300

    старт проекта

---
+++ bin-gen-ssl-keys/gen-cert-x509.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+### openssl req -new -x509 -keyout ca-key -out ca-cert -days 360
---
+++ bin-gen-ssl-keys/gen_key_server.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export JAVA_HOME=/home/lin/apps/jdk13
+export PATH=$JAVA_HOME/bin;$PATH
+# keytool -genkey -keyalg RSA -alias danata_server_alia_key -keystore keystore.jks -storepass 12345678 -validity 360 -keysize 2048
+echo "step-3: Сгенерировать csr-запрос на сертификат"
+keytool -certreq -alias danata_server_alia_key -file danata_server_x509.csr -keystore keystore.jks
+
