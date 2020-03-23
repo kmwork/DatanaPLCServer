43247b6150cefa3a1df2998f816a2678b8bf842c
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-03-23 22:02:33 +0300

    заработала кавка на ssl

--- kafka-config/run-kafka.sh
+++ kafka-config/run-kafka.sh
@@ -2,4 +2,5 @@
 export JAVA_HOME=/home/lin/apps/jdk13
 export PATH=$JAVA_HOME/bin:$PATH
 #### export KAFKA_OPTS="-Djavax.net.debug=all"
+export KAFKA_OPTS="-Djavax.net.debug=all -Dcom.sun.jndi.ldap.object.disableEndpointIdentification=true"
 /home/lin/apps/kafka241/bin/kafka-server-start.sh /home/lin/apps/kafka241/config/server.properties
