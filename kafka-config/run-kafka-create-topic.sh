93236337e5ae61683be5d0a5d91d46286f10cb21
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-03-24 15:13:42 +0300

    старт проекта

--- kafka-config/run-kafka-create-topic.sh
+++ kafka-config/run-kafka-create-topic.sh
@@ -2,8 +2,8 @@
 export JAVA_HOME=/home/lin/apps/jdk13
 export PATH=$JAVA_HOME/bin:$PATH
 #### export KAFKA_OPTS="-Djavax.net.debug=all"
-export KAFKA_OPTS="-Djavax.net.debug=all -Dcom.sun.jndi.ldap.object.disableEndpointIdentification=true"
+export _JAVA_OPTIONS="-Djavax.net.debug=all -Dcom.sun.jndi.ldap.object.disableEndpointIdentification=true"
 #### export KAFKA_OPTS="-Dcom.sun.jndi.ldap.object.disableEndpointIdentification=true"
 export KAFKA_HEAP_OPTS="-Xmx1024M -Xms512m"
 ### bin/kafka-topics.sh --create --bootstrap-server SSL://localhost:9093 --replication-factor 1 --partitions 1 --topic test
-bin/kafka-topics.sh --list --bootstrap-server "SSL://localhost:9093" --command-config ./config/ssl-user-config.properties
+bin/kafka-topics.sh --list --bootstrap-server "SSL://127.0.0.1:9093" --command-config ./config/ssl-user-config.properties
