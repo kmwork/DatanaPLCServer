85900272a4e763054195f8630aee5ad079df939e
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-03-24 11:19:06 +0300

    старт проекта

--- kafka-config/run-kafka-create-topic.sh
+++ kafka-config/run-kafka-create-topic.sh
@@ -4,4 +4,4 @@ export PATH=$JAVA_HOME/bin:$PATH
 #### export KAFKA_OPTS="-Djavax.net.debug=all"
 export KAFKA_OPTS="-Djavax.net.debug=all -Dcom.sun.jndi.ldap.object.disableEndpointIdentification=true"
 export KAFKA_HEAP_OPTS="-Xmx1024M -Xms512m"
-bin/kafka-server-start.sh config/server.properties
+bin/kafka-topics.sh --create --bootstrap-server localhost:9093 --replication-factor 1 --partitions 1 --topic test
