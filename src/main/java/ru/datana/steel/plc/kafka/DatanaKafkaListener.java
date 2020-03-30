f8cd64072459f0a5285746c3939b7a0943075715
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-03-30 16:44:10 +0300

    начинаю делать клиент-серве под PLC (Сименс S7)

--- src/main/java/ru/datana/steel/plc/kafka/DatanaKafkaListener.java
+++ src/main/java/ru/datana/steel/plc/kafka/DatanaKafkaListener.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class DatanaKafkaListener {
 
 
-    @KafkaListener(topics = "${datanaCommonKafkaConfig.metaInfoTopic}", groupId = "{${datanaCommonKafkaConfig.kafkaConsumerMetaGroupId}", containerFactory = "fooKafkaListenerContainerFactory")
+    @KafkaListener(topics = "datanaCommonKafkaConfig.metaInfoTopic", groupId = "datanaCommonKafkaConfig.kafkaConsumerMetaGroupId", containerFactory = "fooKafkaListenerContainerFactory")
     public void listenGroupFoo(String message) {
         log.info("Received Messasge in group 'foo': " + message);
     }
