6a01dfcebc0e99ef6971b6b478c0786964afd2e5
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-04-06 15:16:59 +0300

    Версия 6 на Rest

--- etc/run-server-on-linux.sh
+++ etc/run-server-on-linux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export JAVA_HOME=/home/lin/apps/jdk13
 export PATH=$JAVA_HOME/bin:$PATH
-java -Dapp.dir="/home/lin/work-lanit/Datata-Kafla-Gateway-K7/etc/" -Dfile.encoding=UTF8 -jar Datata-PLC-Server-alfa-1-jar-with-dependencies.jar
+java -Dapp.profile=dev_postgres -Dapp.dir="/home/lin/apps/Danata7" -Dfile.encoding=UTF8 -jar Datata-PLC-Server-alfa-1-jar-with-dependencies.jar
