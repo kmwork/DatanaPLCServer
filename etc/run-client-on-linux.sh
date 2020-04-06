95bb416f0967028e063048a109794e4db45d2219
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-04-07 00:14:53 +0300

    сделал вызов хранимки save

--- etc/run-client-on-linux.sh
+++ etc/run-client-on-linux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export JAVA_HOME=/home/lin/apps/jdk13
 export PATH=$JAVA_HOME/bin:$PATH
-java -Dapp.dir="/home/lin/apps/Danata7" -Dfile.encoding=UTF8 -jar plc-server.jar
+java -Dapp.dir="/home/lin/apps/Danata7" -Dfile.encoding=UTF8 -jar plc-client.jar
