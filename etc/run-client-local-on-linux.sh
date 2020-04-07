67e1d14a5ea67da805f3d36ef566614fc6742dfe
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-04-07 07:22:47 +0300

    подготовка к релизу

--- etc/run-client-local-on-linux.sh
+++ etc/run-client-local-on-linux.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# ************************************************************************
+# *****               Command file for LINUX OS                      *****
+# ***** PLC Client with Postgres, installed on LOCALHOST DATABASE!!! *****
+# ************************************************************************
 export JAVA_HOME=/home/lin/apps/jdk13
 export PATH=$JAVA_HOME/bin:$PATH
 java -Dapp.dir="/home/lin/apps/Datana7" -Dapp.config.file="application-dev_client.yaml" -Dfile.encoding=UTF8 -jar plc-client.jar
