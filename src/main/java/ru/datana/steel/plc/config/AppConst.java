844b33d8442912974b8e9e000b59c7843d0239e1
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-04-08 17:51:43 +0300

    косметика

--- src/main/java/ru/datana/steel/plc/config/AppConst.java
+++ src/main/java/ru/datana/steel/plc/config/AppConst.java
@@ -9,10 +9,7 @@ public class AppConst {
     public static final String SYS_DIR_PROP = "app.dir";
     public static final String ENCODING = "UTF8";
     public final static String FILE_YAML_PROP = "app.config.file";
-
     public final static String RESOURCE_FILE_NAME = "/danata-version.properties";
-
-
     public static final String SUCCESS_LOG_PREFIX = "[App-Успешно] ";
     public static final String ERROR_LOG_PREFIX = "[App-Ошибка] ";
     public static final String APP_LOG_PREFIX = "[App-Danata] ";
