ce33eec229fc250ddb8f9bf71b389c30d9ff4394
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-04-28 14:35:04 +0300

    делаю эмулятор случайных чисел

--- etc/run-client-remote-on-linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# *********************************************************************
-# *****              Command file for LINUX OS                    *****
-# ***** PLC Client with Postgres, installed on REMOVE DATABASE!!! *****
-# *********************************************************************
-export JAVA_HOME=/home/lin/apps/jdk13
-export PATH=$JAVA_HOME/bin:$PATH
-java -Dapp.dir="/home/lin/apps/Datana7" -Dapp.config.file="application-remote_client.yaml" -Dfile.encoding=UTF8 -jar plc-client.jar
