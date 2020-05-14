25ab576f854aa66e768c3223a7b63cdfaec06718
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-05-14 18:46:47 +0300

    шлифование, доводка до ума

--- etc/run-for-camel-on-linux.sh
+++ etc/run-for-camel-on-linux.sh
@@ -2,4 +2,4 @@
 ##### PLC SERVER #####
 export JAVA_HOME=/home/lin/apps/jdk14
 export PATH=$JAVA_HOME/bin:$PATH
-java -Dapp.dir="/home/lin/apps/DatanaCamel" -Dfile.encoding=UTF8 -jar plc-camel.jar
+java -Dapp.dir="/home/lin/apps/DatanaCamel" -Dfile.encoding=UTF8 -jar plc-camel-001.jar
