8a8cffecb7b63346a1ebe20cb541ade910d2c4c4
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-06-19 15:48:56 +0300

    делаю pipeline

--- for-jenkins/k4-datana-ci-cd.groovy
+++ for-jenkins/k4-datana-ci-cd.groovy
@@ -26,24 +26,26 @@ pipeline {
 
         stage('Telegram step') {
             steps {
-                gitVar = git(branch: 'Generator_REST_BY_SIEMENS', credentialsId: 'KostyaDatanaV5', url: 'git@gitlab.dds.lanit.ru:datana_smart/tools-adapters.git')
-                /* echo gitVar.GIT_COMMIT
-                Fields:
+                script {
+                    gitVar = git(branch: 'Generator_REST_BY_SIEMENS', credentialsId: 'KostyaDatanaV5', url: 'git@gitlab.dds.lanit.ru:datana_smart/tools-adapters.git')
+                    /* echo gitVar.GIT_COMMIT
+                        Fields:
 
-                GIT_AUTHOR_EMAIL
-                GIT_AUTHOR_NAME
-                GIT_BRANCH
-                GIT_COMMIT
-                GIT_COMMITTER_EMAIL
-                GIT_COMMITTER_NAME
-                GIT_LOCAL_BRANCH
-                GIT_PREVIOUS_COMMIT
-                GIT_PREVIOUS_SUCCESSFUL_COMMIT
-                GIT_URL */
-                sh 'curl - x socks5://proxyuser:secure@94.177.216.245:777 -X POST "https://api.telegram.org/bot1180854473:AAG1BHnbcM4oRRZW2-DKbZMYD2WqkDtUesU/sendMessage?chat_id=-1001325011128&parse_mode=HTML&text=Собрал. '
-                +'GIT_COMMITTER_NAME = ' + gitVar.GIT_COMMITTER_NAME
-                +'GIT_AUTHOR_NAME = ' + gitVar.GIT_AUTHOR_NAME
-                + '"'
+                        GIT_AUTHOR_EMAIL
+                        GIT_AUTHOR_NAME
+                        GIT_BRANCH
+                        GIT_COMMIT
+                        GIT_COMMITTER_EMAIL
+                        GIT_COMMITTER_NAME
+                        GIT_LOCAL_BRANCH
+                        GIT_PREVIOUS_COMMIT
+                        GIT_PREVIOUS_SUCCESSFUL_COMMIT
+                        GIT_URL */
+                    sh 'curl - x socks5://proxyuser:secure@94.177.216.245:777 -X POST "https://api.telegram.org/bot1180854473:AAG1BHnbcM4oRRZW2-DKbZMYD2WqkDtUesU/sendMessage?chat_id=-1001325011128&parse_mode=HTML&text=Собрал. '
+                    +'GIT_COMMITTER_NAME = ' + gitVar.GIT_COMMITTER_NAME
+                    +'GIT_AUTHOR_NAME = ' + gitVar.GIT_AUTHOR_NAME
+                    +'"'
+                }
             }
         }
     }
