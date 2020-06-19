345b5f008e6f59aab3df71d39cf1a48d682cded6
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-06-19 15:23:30 +0300

    делают бота для уведомлений в телеграм

--- for-jenkins/k4-datana-ci-cd.groovy
+++ for-jenkins/k4-datana-ci-cd.groovy
@@ -11,12 +11,12 @@ pipeline {
         timestamps()
     }
     stages {
-        stage("k2 - Checkout") {
+        stage('k2 - Checkout') {
             steps {
                 checkout([$class: 'GitSCM', branches: [[name: 'Generator_REST_BY_SIEMENS']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'kakunin', url: 'https://gitlab.dds.lanit.ru']]])
             }
         }
-        stage("k2 - Build") {
+        stage('k2 - Build') {
             steps {
                 withMaven(maven: 'maven3') {
                     sh "mvn clean compile package spring-boot:repackage -P plcServer "
@@ -24,9 +24,9 @@ pipeline {
             }
         }
 
-        stage("Telegram step") {
+        stage('Telegram step') {
             steps {
-                gitVar = git(branch: 'Generator_REST_BY_SIEMENS', credentialsId: 'KostyaDatanaV5', url: "git@gitlab.dds.lanit.ru:datana_smart/tools-adapters.git")
+                gitVar = git(branch: 'Generator_REST_BY_SIEMENS', credentialsId: 'KostyaDatanaV5', url: 'git@gitlab.dds.lanit.ru:datana_smart/tools-adapters.git')
                 /* echo gitVar.GIT_COMMIT
                 Fields:
 
@@ -40,9 +40,10 @@ pipeline {
                 GIT_PREVIOUS_COMMIT
                 GIT_PREVIOUS_SUCCESSFUL_COMMIT
                 GIT_URL */
-                sh "curl - x socks5://proxyuser:secure@94.177.216.245:777 -X POST \"https://api.telegram.org/bot1180854473:AAG1BHnbcM4oRRZW2-DKbZMYD2WqkDtUesU/sendMessage?chat_id=-1001325011128&parse_mode=HTML&text=Собрал. "
-                +"GIT_COMMITTER_NAME = " + gitVar.GIT_COMMITTER_NAME
-                +"GIT_AUTHOR_NAME = " + gitVar.GIT_AUTHOR_NAME
+                sh 'curl - x socks5://proxyuser:secure@94.177.216.245:777 -X POST "https://api.telegram.org/bot1180854473:AAG1BHnbcM4oRRZW2-DKbZMYD2WqkDtUesU/sendMessage?chat_id=-1001325011128&parse_mode=HTML&text=Собрал. '
+                +'GIT_COMMITTER_NAME = ' + gitVar.GIT_COMMITTER_NAME
+                +'GIT_AUTHOR_NAME = ' + gitVar.GIT_AUTHOR_NAME
+                + '"'
             }
         }
     }
