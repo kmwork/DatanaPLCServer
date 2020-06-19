c6cd99eb7453510af04b1ad8403f5c3ef466f154
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-06-19 14:07:14 +0300

    делают бота для уведомлений в телеграм

--- for-jenkins/k4-datana-ci-cd.groovy
+++ for-jenkins/k4-datana-ci-cd.groovy
@@ -11,14 +11,38 @@ pipeline {
         timestamps()
     }
     stages {
-        stage("First step") {
+        stage("k2 - Checkout") {
             steps {
-                sh 'ssh root@ub1 \'hostname\''
+                checkout([$class: 'GitSCM', branches: [[name: 'Generator_REST_BY_SIEMENS']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'kakunin', url: 'https://gitlab.dds.lanit.ru']]])
             }
         }
-        stage("Second step") {
+        stage("k2 - Build") {
             steps {
-                sh 'ssh root@ub1 \'uptime\''
+                withMaven(maven: 'maven3') {
+                    sh "mvn clean compile package spring-boot:repackage -P plcServer "
+                }
+            }
+        }
+
+        stage("Telegram step") {
+            steps {
+                gitVar = git(branch: 'Generator_REST_BY_SIEMENS', credentialsId: 'KostyaDatanaV5', url: "git@gitlab.dds.lanit.ru:datana_smart/tools-adapters.git")
+                /* echo gitVar.GIT_COMMIT
+                Fields:
+
+                GIT_AUTHOR_EMAIL
+                GIT_AUTHOR_NAME
+                GIT_BRANCH
+                GIT_COMMIT
+                GIT_COMMITTER_EMAIL
+                GIT_COMMITTER_NAME
+                GIT_LOCAL_BRANCH
+                GIT_PREVIOUS_COMMIT
+                GIT_PREVIOUS_SUCCESSFUL_COMMIT
+                GIT_URL */
+                sh "curl - x socks5://proxyuser:secure@94.177.216.245:777 -X POST \"https://api.telegram.org/bot1180854473:AAG1BHnbcM4oRRZW2-DKbZMYD2WqkDtUesU/sendMessage?chat_id=-1001325011128&parse_mode=HTML&text=Собрал. "
+                +"GIT_COMMITTER_NAME = " + gitVar.GIT_COMMITTER_NAME
+                +"GIT_AUTHOR_NAME = " + gitVar.GIT_AUTHOR_NAME
             }
         }
     }
