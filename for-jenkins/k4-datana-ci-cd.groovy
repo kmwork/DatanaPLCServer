c03ceb1230789ed8ae18bd90ce65f923bc290eb4
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-06-21 01:16:23 +0300

    делаю pipeline (as node 6)

--- for-jenkins/k4-datana-ci-cd.groovy
+++ for-jenkins/k4-datana-ci-cd.groovy
@@ -11,16 +11,18 @@ node {
     def constGitCredentialsId
     def constGIT_COMMITTER_NAME
     def constGIT_AUTHOR_NAME
-    def constMVN_HOME = '/home/lin/apps/apache-maven-3.5.4'
+    def constMVN_HOME
     stage('step-0: Init') {
         constGitBranch = 'Generator_REST_BY_SIEMENS'
         constGitUrl = 'git@gitlab.dds.lanit.ru:datana_smart/tools-adapters.git'
         constGitCredentialsId = 'kostya5'
-        gitVar = git(branch: constGitBranch, credentialsId: constGitCredentialsId, url: constGitUrl)
+        def gitVar = git(branch: constGitBranch, credentialsId: constGitCredentialsId, url: constGitUrl)
         constGIT_COMMITTER_NAME = gitVar.GIT_COMMITTER_NAME
         constGIT_AUTHOR_NAME = gitVar.GIT_AUTHOR_NAME
         constMVN_HOME = '/home/lin/apps/apache-maven-3.5.4'
         echo "User:" + constGitCredentialsId + "\n" + "GitBranch: " + constGitBranch
+        constJAVA_HOME = '/home/lin/apps/jdk13'
+        env.PATH = "$constMVN_HOME/bin:$constJAVA_HOME/bin:$PATH"
     }
     stage('step-1: Checkout') {
         echo 'Building'
@@ -28,11 +30,14 @@ node {
     }
 
     stage('step-2: Build by maven') {
-        sh "$constMVN_HOME/bin/mvn clean compile package spring-boot:repackage -P plcServer"
+        sh "mvn clean compile package spring-boot:repackage -P plcServer"
     }
 
     stage('step-3: Telegram step') {
-        sh "curl - x socks5://proxyuser:secure@94.177.216.245:777 -X POST \"https://api.telegram.org/bot1180854473:AAG1BHnbcM4oRRZW2-DKbZMYD2WqkDtUesU/sendMessage?chat_id=-1001325011128&parse_mode=HTML&text=Builed.+GIT_COMMITTER_NAME+=+$constGIT_COMMITTER_NAME,+GIT_AUTHOR_NAME+=+$constGIT_AUTHOR_NAME\""
+        sh "curl -x socks5://proxyuser:secure@94.177.216.245:777 -X POST \"https://api.telegram.org/bot1180854473:AAG1BHnbcM4oRRZW2-DKbZMYD2WqkDtUesU/sendMessage?chat_id=-1001325011128&parse_mode=HTML&text=Builed."
+        +"+GIT_COMMITTER_NAME+=+" + constGIT_COMMITTER_NAME
+        +"+GIT_AUTHOR_NAME+=+" constGIT_AUTHOR_NAME
+        +"\""
     }
 }
 
