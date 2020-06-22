7b1db48b7eeb246c16cb7deaed95b550a6073ec2
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-06-22 17:44:21 +0300

    делаю pipeline - доделываю докеры (попилинг-проба)

--- for-jenkins/k4-datana-ci-cd.groovy
+++ for-jenkins/k4-datana-ci-cd.groovy
@@ -17,6 +17,7 @@ node {
     def constDockerName
     def constDockerRegistryLogin
     def constDockerRegistryPassword
+    def constDockerDomain
     stage('step-0: Init') {
         constGitBranch = 'Generator_REST_BY_SIEMENS'
         constGitUrl = 'git@gitlab.dds.lanit.ru:datana_smart/tools-adapters.git'
@@ -27,7 +28,8 @@ node {
         constJAVA_HOME = '/home/lin/apps/jdk13'
         env.PATH = "$constMVN_HOME/bin:$constJAVA_HOME/bin:$PATH"
         //constDockerRegistry = "https://hub.docker.com/repository/docker/kmtemp/datana"
-        constDockerRegistry = "https://registry.hub.docker.com"
+        constDockerDomain = "registry.hub.docker.com"
+        constDockerRegistry = "https:/$constDockerDomain"
 
         constTelegramURL = "https://api.telegram.org/bot1180854473:AAG1BHnbcM4oRRZW2-DKbZMYD2WqkDtUesU/sendMessage?chat_id=-1001325011128&parse_mode=HTML"
         constProxyTelegram = "socks5://proxyuser:secure@94.177.216.245:777"
@@ -52,22 +54,21 @@ node {
     }
 
     stage('step-3: Docker build') {
-        sh "docker build -t=$constDockerName/datana ."
+        sh "docker build $constDockerName/$constDockerDomain/datana:latest ."
     }
+//
+//    stage('step-4: Docker remove') {
+//        sh "docker stop $constDockerName || true && docker rm $constDockerName || true"
+//    }
 
-    stage('step-4: Docker remove') {
-        sh "docker stop $constDockerName || true && docker rm $constDockerName || true"
-    }
-
-
-    stage('step-5: Docker create') {
-        sh "docker create $constDockerName/datana"
-    }
+//
+//    stage('step-5: Docker create') {
+//        sh "docker create $constDockerName/datana"
+//    }
 
     stage('step-6: Docker push') {
         sh "cat /home/lin/apps/datana-docker-secret/rep-password.txt | docker login --password-stdin --username=$constDockerName $constDockerRegistry"
-
-        sh "docker push $constDockerName/datana"
+        sh "docker push $constDockerName/$constDockerDomain/datana:latest"
     }
 
 
