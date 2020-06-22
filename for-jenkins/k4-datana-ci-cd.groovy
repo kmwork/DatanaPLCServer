13406529b289a08edd4b122c28fc58173ed0400a
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-06-22 15:19:05 +0300

    делаю pipeline - доделываю докеры (попилинг)

--- for-jenkins/k4-datana-ci-cd.groovy
+++ for-jenkins/k4-datana-ci-cd.groovy
@@ -13,6 +13,8 @@ node {
     def gitVar
     def constTelegramURL
     def constProxyTelegram
+    def constDockerRegistry
+    def constDockerName
     stage('step-0: Init') {
         constGitBranch = 'Generator_REST_BY_SIEMENS'
         constGitUrl = 'git@gitlab.dds.lanit.ru:datana_smart/tools-adapters.git'
@@ -22,9 +24,11 @@ node {
         echo "User:" + constGitCredentialsId + "\n" + "GitBranch: " + constGitBranch
         constJAVA_HOME = '/home/lin/apps/jdk13'
         env.PATH = "$constMVN_HOME/bin:$constJAVA_HOME/bin:$PATH"
+        constDockerRegistry = "https://hub.docker.com/repository/docker/kmtemp/datana"
 
         constTelegramURL = "https://api.telegram.org/bot1180854473:AAG1BHnbcM4oRRZW2-DKbZMYD2WqkDtUesU/sendMessage?chat_id=-1001325011128&parse_mode=HTML"
         constProxyTelegram = "socks5://proxyuser:secure@94.177.216.245:777"
+        constDockerName = "kmtemp"
         echo "[PARAM] PATH=$PATH"
         echo "[PARAM] gitVar=$gitVar"
         echo "-----------------------------------"
@@ -41,15 +45,24 @@ node {
     }
 
     stage('step-3: Docker build') {
-        sh "docker build -t=kmtemp/datana ."
+        sh "docker build -t=$constDockerName/datana ."
     }
 
     stage('step-4: Docker remove') {
-        sh "docker container rm kmtemp"
+        sh "docker stop $constDockerName || true && docker rm $constDockerName || true"
     }
 
 
-    stage('step-5: Telegram step') {
+    stage('step-5: Docker create') {
+        sh "docker create $constDockerName/datana"
+    }
+
+    stage('step-6: Docker push') {
+        sh "docker push $constDockerRegistry datana:latest"
+    }
+
+
+    stage('step-7: Telegram step') {
         def DatanaAuthor = sh script: "git show -s --pretty=\"%an <%ae>\" ${gitVar.GIT_COMMIT}", returnStdout: true
         DatanaAuthor = DatanaAuthor.replace("@", " ")
         DatanaAuthor = DatanaAuthor.replace("<", " ")
