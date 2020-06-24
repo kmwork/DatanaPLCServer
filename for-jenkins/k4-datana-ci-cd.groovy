9475a46f56da89d20f7556ec3037e752fb270e28
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-06-24 17:48:53 +0300

    делаю pipeline - доделываю докеры

--- for-jenkins/k4-datana-ci-cd.groovy
+++ for-jenkins/k4-datana-ci-cd.groovy
@@ -108,6 +108,7 @@ try {
 
         stage('step-5: Docker create') {
             sh "docker create $env.constDockerName/$env.constDockerTag"
+            sh "docker run -d -v /var/run/docker.sock:/var/run/docker.sock -v \$(which docker):/usr/bin/docker -p 9999:8080 $env.constDockerName/$env.constDockerTag"
             sh "docker tag $env.constDockerName/$env.constDockerTag $env.constDockerDomain/$env.constDockerName/$env.constDockerTag:$env.constDockerImageVersion"
         }
 
